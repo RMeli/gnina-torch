0ed9e82a7c8c1f29ca719cecf5d9c45904c25e3b
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-10-19 21:50:18 +0100

    apply black formatting

--- gnina/__init__.py
+++ gnina/__init__.py
@@ -5,7 +5,8 @@ from .gnina import *
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
--- gnina/tests/test_gnina.py
+++ gnina/tests/test_gnina.py
@@ -7,5 +7,6 @@ import molgrid
 def test_gnina_imported():
     assert "gnina" in sys.modules
 
+
 def test_molgrid_imported():
     assert "molgrid" in sys.modules

d11444b1204ed54e6267fbd6be9b17487accec07
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-10-19 21:16:00 +0100

    test molgrid import

--- gnina/tests/test_gnina.py
+++ gnina/tests/test_gnina.py
@@ -1,15 +1,11 @@
-"""
-Unit and regression test for the gnina package.
-"""
-
-# Import package, test suite, and other packages as needed
 import sys
 
-import pytest
-
 import gnina
+import molgrid
 
 
 def test_gnina_imported():
-    """Sample test, will always pass so long as import statement worked."""
     assert "gnina" in sys.modules
+
+def test_molgrid_imported():
+    assert "molgrid" in sys.modules

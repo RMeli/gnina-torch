0c44d05e2c50ac649244a93e8773b5316454719d
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-11-21 21:45:06 +0000

    improve affinity loss

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -8,7 +8,7 @@ from gnina import losses
 def test_affinity_loss_perfect_predictions(device):
     criterion = losses.AffinityLoss()
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     assert criterion(target, target).item() == pytest.approx(0.0)
 
@@ -16,72 +16,116 @@ def test_affinity_loss_perfect_predictions(device):
 def test_affinity_loss_zero_affinity(device):
     criterion = losses.AffinityLoss()
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     # Predictions where target is zero are ignored
-    predicted = torch.tensor([1.0, 1.1, 2.2, 3.3, 4.4])
+    predicted = torch.tensor([1.0, 1.1, 2.2, 3.3, 4.4], device=device)
 
     assert criterion(predicted, target).item() == pytest.approx(0.0)
 
 
 def test_affinity_loss_wrong_predicted_affinity_good_pose_sum(device):
-    criterion = losses.AffinityLoss(reduction="sum")
+    criterion = losses.AffinityLoss(reduction="sum", pseudo_huber=True, scale=100.0)
 
-    diff = 0.1
+    diff = 1.5
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     # Affinity error with good pose
-    predicted = torch.tensor([0.0, 1.1 + diff, 2.2, 3.3, 4.4])
+    predicted = torch.tensor([0.0, 1.1 + diff, 2.2, 3.3, 4.4], device=device)
 
     assert criterion(predicted, target).item() == pytest.approx(
-        np.sqrt(1.0 + diff ** 2) - 1.0, abs=1e-6
+        100 * (np.sqrt(1.0 + diff ** 2) - 1.0)
     )
 
 
-def test_affinity_loss_wrong_predicted_affinity_good_pose_mean(device):
-    criterion = losses.AffinityLoss(reduction="mean")
+def test_affinity_loss_wrong_predicted_affinity_good_pose_mean_PH(device):
+    criterion = losses.AffinityLoss(reduction="mean", pseudo_huber=True, scale=100.0)
 
-    diff = 0.1
+    diff = 1.5
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     # Affinity error with good pose
-    predicted = torch.tensor([0.0, 1.1 + diff, 2.2, 3.3, 4.4])
+    predicted = torch.tensor([0.0, 1.1 + diff, 2.2, 3.3, 4.4], device=device)
 
     # Divide by the lenght of the target to get mean loss
-    expected_loss = (np.sqrt(1.0 + diff ** 2) - 1.0) / len(target)
+    expected_loss = 100 * (np.sqrt(1.0 + diff ** 2) - 1.0) / len(target)
 
-    assert criterion(predicted, target).item() == pytest.approx(expected_loss, abs=1e-6)
+    assert criterion(predicted, target).item() == pytest.approx(expected_loss)
 
 
-def test_affinity_loss_overestimated_predicted_affinity_bad_pose_sum(device):
-    criterion = losses.AffinityLoss(reduction="sum")
+def test_affinity_loss_overestimated_predicted_affinity_bad_pose_sum_PH(device):
+    criterion = losses.AffinityLoss(reduction="sum", pseudo_huber=True, scale=100.0)
 
-    diff = 0.1
+    diff = 1.5
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     # Affinity error with bad pose
     # Affinity is overestimated
-    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 + diff, 4.4])
+    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 + diff, 4.4], device=device)
 
-    assert criterion(predicted, target).item() == pytest.approx(
-        np.sqrt(1.0 + diff ** 2) - 1.0, abs=1e-6
-    )
+    expected_loss = 100 * (np.sqrt(1.0 + diff ** 2) - 1.0)
+
+    assert criterion(predicted, target).item() == pytest.approx(expected_loss)
 
 
-def test_affinity_loss_underestimated_predicted_affinity_bad_pose_sum(device):
-    criterion = losses.AffinityLoss(reduction="sum")
+def test_affinity_loss_underestimated_predicted_affinity_bad_pose_sum_PH(device):
+    criterion = losses.AffinityLoss(reduction="sum", pseudo_huber=True, scale=10.0)
 
-    diff = 0.1
+    diff = 1.5
 
-    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4])
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
 
     # Affinity error with bad pose
     # Affinity is underestimated, which is what we want
-    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 - diff, 4.4])
+    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 - diff, 4.4], device=device)
 
     # Underestimated affinity for a bad pose does not contribute to the loss
-    # TODO: should contribute with add diff_for_zero parameter
+    assert criterion(predicted, target).item() == pytest.approx(0.0)
+
+
+def test_affinity_loss_wrong_predicted_affinity_good_pose_mean_L2(device):
+    # Delta parameter only affects pseudo-Huber loss
+    criterion = losses.AffinityLoss(reduction="mean", pseudo_huber=False, delta=4.0)
+
+    diff = 1.5
+
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
+
+    # Affinity error with good pose
+    predicted = torch.tensor([0.0, 1.1 + diff, 2.2, 3.3, 4.4], device=device)
+
+    assert criterion(predicted, target).item() == pytest.approx(diff ** 2 / len(target))
+
+
+def test_affinity_loss_overestimated_predicted_affinity_bad_pose_sum_L2(device):
+    # Delta parameter only affects pseudo-Huber loss
+    criterion = losses.AffinityLoss(reduction="sum", pseudo_huber=False, delta=4.0)
+
+    diff = 1.5
+
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
+
+    # Affinity error with bad pose
+    # Affinity is overestimated
+    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 + diff, 4.4], device=device)
+
+    assert criterion(predicted, target).item() == pytest.approx(diff ** 2)
+
+
+def test_affinity_loss_underestimated_predicted_affinity_bad_pose_sum_L2(device):
+    # Delta parameter only affects pseudo-Huber loss
+    criterion = losses.AffinityLoss(reduction="sum", pseudo_huber=False, delta=4.0)
+
+    diff = 1.5
+
+    target = torch.tensor([0.0, 1.1, 2.2, -3.3, -4.4], device=device)
+
+    # Affinity error with bad pose
+    # Affinity is underestimated, which is what we want
+    predicted = torch.tensor([0.0, 1.1, 2.2, 3.3 - diff, 4.4], device=device)
+
+    # Underestimated affinity for a bad pose does not contribute to the loss
     assert criterion(predicted, target).item() == pytest.approx(0.0)

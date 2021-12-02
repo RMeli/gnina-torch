1d96aa5b5a56516e35aee6df5745d85b1b28430d
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-12-02 15:07:02 +0000

    cleanup __len__ method in example provider

--- gnina/dataloaders.py
+++ gnina/dataloaders.py
@@ -93,34 +93,6 @@ class GriddedExamplesLoader:
         self.batch_idx = 0
         self.last_epoch = False
 
-    # TODO: Check this is what we want
-    # TODO: By default, epoch length is defined by len(data)
-    # TODO: If data is a finite data iterator with unknown length epoch_length
-    # TODO: will be automatically determined when data iterator is exhausted.
-    # def __len__(self):
-    #     """
-    #     Return length of the epoch (number of examples).
-
-    #     Notes
-    #     -----
-    #     The number of examples per epoch depends on the :code:`molgrid.IterationScheme`
-    #     used. Without balancing nor stratification, the number of examples per epoch is
-    #     the same for :code:`molgrid.IterationScheme.SmalleEpoch` and
-    #     :code:`molgrid.IterationScheme.LargeEpoch`. For balanced sampling, which sample
-    #     the same number of positive and negative examples, the number of examples in a
-    #     small epoch (examples seen at most once) is twice the size of the minority class
-    #     while for a large epoch (examples seen at least once) it is twice the size of
-    #     the majority class.
-    #     """
-    #     settings = self.example_provider.settings()
-
-    #     if settings.iteration_scheme == molgrid.IterationScheme.SmallEpoch:
-    #         return self.example_provider.small_epoch_size()
-    #     elif settings.iteration_scheme == molgrid.IterationScheme.LargeEpoch:
-    #         return self.example_provider.large_epoch_size()
-    #     else:
-    #         raise ValueError("Unknown iteration scheme {settings.iteration_scheme}.")
-
     def __next__(self):
         """
         Get next batch of gridded examples and corresponding labels.
